03699f669ec17b480728eddb873a112b48176f32
esoto76 <elpasocolorartist@yahoo.com>
2022-08-11 08:02:16 -0600

    feat: add navbar title text and cleanup

--- src/components/Navbox.vue
+++ src/components/Navbox.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import LogoSvg from "./LogoSvg.vue";
+import { useMainStore } from "@/stores/main";
+const mainStore = useMainStore();
 </script>
 <template>
   <div class="card-con navbox">
     <div class="navbar">
       <div class="navbar-bx is-logo">
         <LogoSvg />
+        <h1 class="title-bx">{{ mainStore.title }}</h1>
       </div>
     </div>
   </div>
--- src/stores/main.ts
+++ src/stores/main.ts
@@ -2,4 +2,7 @@ import { defineStore } from "pinia";
 
 export const useMainStore = defineStore({
   id: "main",
+  state: () => ({
+    title: "real estate and property management",
+  }),
 });
