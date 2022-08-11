ddd23a29a0d6a758c3e5098d10d74952bb68f740
esoto76 <elpasocolorartist@yahoo.com>
2022-08-11 07:44:41 -0600

    refactor: cleanup and optimization

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,16 +7,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./components/icons/IconTooling.vue')['default']
     LogoSvg: typeof import('./components/LogoSvg.vue')['default']
     Navbox: typeof import('./components/Navbox.vue')['default']
-    NavBox: typeof import('./components/NavBox.vue')['default']
-    TheWelcome: typeof import('./components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./components/WelcomeItem.vue')['default']
   }
 }
