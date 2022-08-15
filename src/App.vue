b20d5d4209c584b2e83e90c14265844be3942c9f
esoto76 <elpasocolorartist@yahoo.com>
2022-08-15 11:20:20 -0600

    chore: updates and optimization

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
 import EhoSvg from "./components/EhoSvg.vue";
 </script>
 
 <template>
   <Header />
-  <div class="card-con content-bx">content box</div>
+  <div class="content-bx">
+    <BannerBox />
+  </div>
   <footer class="card-con foot-bx">
     <div class="footer-bx is-eho"><EhoSvg /></div>
   </footer>
