44515f543d4d529013c24021dcffef0d85d3009d
esoto76 <elpasocolorartist@yahoo.com>
2022-08-15 18:40:58 -0600

    feat: add rentals page

---
+++ src/pages/rentals.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="page is-rentals">
+    <div class="page-inner">
+      <iframe
+        loading="lazy"
+        src="https://benchmarkrentals.managebuilding.com/Resident/Public/Rentals?hidenav=true"
+        scrolling="yes"
+        crossorigin="anonymous"
+      ></iframe>
+    </div>
+  </section>
+</template>
