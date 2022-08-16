05747f4078ed05f2ad70aaa8b720bc7224a6b137
esoto76 <elpasocolorartist@yahoo.com>
2022-08-15 19:01:01 -0600

    feat: add contact form

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -2,6 +2,34 @@
 
 <template>
   <section class="page is-contact">
-    <div class="page-inner">contact</div>
+    <div class="page-inner">
+      <div class="contact-form-bx">
+        <form class="contact-form">
+          <div class="contact-form-title">contact us</div>
+          <div class="contact-form-entry-bx first-name">
+            <label for="firstName"> first name</label>
+            <input type="text" name="firstName" />
+          </div>
+          <div class="contact-form-entry-bx last-name">
+            <label for="lastName">last name</label>
+            <input type="text" name="lastName" />
+          </div>
+          <div class="contact-form-entry-bx email">
+            <label for="email">email</label> <input type="email" name="email" />
+          </div>
+          <div class="contact-form-entry-bx phone">
+            <label for="phone">phone</label> <input type="tel" name="phone" />
+          </div>
+          <div class="contact-form-entry-bx subject">
+            <label for="subject">subject</label>
+            <input type="text" name="subject" />
+          </div>
+          <div class="contact-form-entry-bx comments">
+            <label for="comments">comments</label>
+            <textarea type="text" name="comments"></textarea>
+          </div>
+        </form>
+      </div>
+    </div>
   </section>
 </template>
