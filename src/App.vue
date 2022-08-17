7a5f15af52d9724e6312009fc02e064943516968
esoto76 <elpasocolorartist@yahoo.com>
2022-08-17 13:35:03 -0600

    feat: add window.onscroll and scroll shadow

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const winScroll = ref(0);
+
+const IsScroll = computed(() => (winScroll.value > 0 ? "is-scroll" : ""));
+
+onMounted(() => {
+  window.onscroll = () => {
+    winScroll.value = window.scrollY;
+    window.onscroll = () => (winScroll.value = window.scrollY);
+  };
+});
+</script>
 
 <template>
-  <Header />
+  <Header :class="[IsScroll]" />
   <div class="content-bx">
     <div class="content-top-bx">
       <Banner />
